46fdc8635f8300d15c8127a0f6e8bb77443de66b
Lapin <juste.lapin@gmail.com>
2011-05-19 11:48:14 +0200

    Change the read of 'exp1.csv'
    The reading of database 'exp1.csv' will be needed by each file of first experimentation analysis.
    It was read in the analysis file but now, it is in 'Rinit'.

--- files/exp1-time-residue.r
+++ files/exp1-time-residue.r
@@ -2,11 +2,8 @@
 setwd("./files/")
 source("Rinit")
 out_file = get_outname(commandArgs())
-print(out_file)
 
-dat = read.table("exp1.csv", h=T)
-
-bp = boxplot(dat$Time~dat$Pattern, range=2, plot=FALSE)
+bp = boxplot(exp1_dat$Time~exp1_dat$Pattern, range=2, plot=FALSE)
 titles = c("\\myresidue{1}","\\myresidue{2}","\\myresidue{3}","\\myresidue{4}","\\myresidue{5}","\\myresidue{6}","\\myresidue{7}","\\myresidue{8}","\\myresidue{9}","\\myresidue{10}")
 names(bp$stats) = titles
 write.table(bp$stats,
