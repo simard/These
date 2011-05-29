c2f2dc413ab4fa1327f32088bf46c4f85a024498
Lapin <juste.lapin@gmail.com>
2011-05-30 01:21:41 +0200

    Self review on experimentation 1
    Read all the chapter and do some corrections.
    Verify the coherence.

---
+++ files/exp1-age.r
@@ -0,0 +1,18 @@
+#!/usr/bin/env Rscript
+setwd("./files/")
+source("Rinit")
+out.file = get.outname(commandArgs())
+
+# Generate an empty file to prevent compilation from 'Makefile' file
+write(
+	  "",
+	  file = out.file
+	  )
+
+summary.age = global.summary(dat.exp1$subject.age)
+out.summary.age = summary2tex(summary.age)
+out.file.age = gsub(".csv", ".tex", out.file)
+write(
+	  out.summary.age,
+	  file = out.file.age
+	  )
