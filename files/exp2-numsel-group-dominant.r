1ef26bde862278e00b7eba7d1221d437fd929193
Lapin <juste.lapin@gmail.com>
2011-07-03 17:45:15 +0200

    Number of selection's graphic in experimentation 2
    Add a graphic about number of selection for either dominant hand and non-dominant hand in monome and binome for the second experimentation.

---
+++ files/exp2-numsel-group-dominant.r
@@ -0,0 +1,70 @@
+#!/usr/bin/env Rscript
+setwd("./files/")
+source("Rinit")
+out.file.boxplot = get.outname(commandArgs())
+
+dat.exp2.monome = dat.exp2[dat.exp2$group.number == 1, ]
+dat.exp2.monome.dominant = data.frame(
+									  total.selection.number=dat.exp2.monome$total.leader.selection.number, 
+									  group.number=dat.exp2.monome$group.number,
+									  is.slave=rep(FALSE,length(dat.exp2.monome$group.number))
+									  )
+dat.exp2.monome.nondominant = data.frame(
+									  total.selection.number=dat.exp2.monome$total.slave.selection.number, 
+									  group.number=dat.exp2.monome$group.number,
+									  is.slave=rep(TRUE,length(dat.exp2.monome$group.number))
+									  )
+dat.exp2.binome = dat.exp2[dat.exp2$group.number == 2, ]
+dat.exp2.binome.dominant = data.frame(
+									  total.selection.number=dat.exp2.binome$total.leader.selection.number + dat.exp2.binome$total.slave.selection.number,
+									  group.number=dat.exp2.binome$group.number,
+									  is.slave=rep(FALSE,length(dat.exp2.binome$group.number))
+									  )
+dat.exp2.binome.nondominant = data.frame(
+									  total.selection.number=0,
+									  group.number=2,
+									  is.slave=TRUE
+									  )
+dat.selection.number = rbind(dat.exp2.monome.dominant, dat.exp2.monome.nondominant, dat.exp2.binome.dominant, dat.exp2.binome.nondominant)
+dat.selection.number.dominant = dat.selection.number[dat.selection.number$is.slave == FALSE, ]
+bp.selection.number.dominant = boxplot(dat.selection.number.dominant$total.selection.number ~ dat.selection.number.dominant$group.number, plot=FALSE)
+colnames(bp.selection.number.dominant$stats) = c("\\myglosnl{glo-Monome}","\\myglosnl{glo-Binome}")
+write.table(bp.selection.number.dominant$stats,
+			file = out.file.boxplot,
+			quote = FALSE,
+			sep = ",",
+			eol = "\n",
+			dec = ".",
+			row.names = FALSE,
+			col.names = TRUE)
+dat.selection.number.nondominant = dat.selection.number[dat.selection.number$is.slave == TRUE, ]
+bp.selection.number.nondominant = boxplot(dat.selection.number.nondominant$total.selection.number ~ dat.selection.number.nondominant$group.number, plot=FALSE)
+write.table(bp.selection.number.nondominant$stats,
+			file = out.file.boxplot,
+			quote = FALSE,
+			sep = ",",
+			eol = "\n",
+			dec = ".",
+			row.names = FALSE,
+			col.names = FALSE,
+			append = TRUE)
+
+dat.exp2.monome.cumulative = data.frame(
+									  total.selection.number=dat.exp2.monome$total.leader.selection.number + dat.exp2.monome$total.slave.selection.number,
+									  group.number=dat.exp2.monome$group.number
+									  )
+dat.exp2.binome.cumulative = data.frame(
+									  total.selection.number=dat.exp2.binome$total.leader.selection.number + dat.exp2.binome$total.slave.selection.number,
+									  group.number=dat.exp2.binome$group.number
+									  )
+dat.exp2.cumulative = rbind(dat.exp2.monome.cumulative, dat.exp2.binome.cumulative)
+shapiro = shapiro.test(dat.exp2.cumulative$total.selection.number)
+dat.exp2.cumulative.levene = na.omit(data.frame(total.selection.number=dat.exp2.cumulative$total.selection.number, group.number=dat.exp2.cumulative$group.number))
+levene = levene.test(dat.exp2.cumulative.levene$total.selection.number, dat.exp2.cumulative.levene$group.number)
+kruskal = kruskal.test(dat.exp2.cumulative$total.selection.number ~ dat.exp2.cumulative$group.number)
+out.anova = kruskal2tex(kruskal)
+out.file.anova = gsub(".csv", "-anova-cumulative.tex", out.file.boxplot)
+write(
+	  out.anova,
+	  file = out.file.anova
+	  )
