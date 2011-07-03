6444b9d45605198fcc4e50152773b4872ef68cd1
Lapin <juste.lapin@gmail.com>
2011-07-04 01:35:59 +0200

    Fix in R script with undesired lines
    Some lines about a Friedman test should be erased.

--- files/exp2-time-group.r
+++ files/exp2-time-group.r
@@ -19,10 +19,6 @@ shapiro = shapiro.test(dat.exp2$time.completion)
 dat.exp2.levene = na.omit(data.frame(time.completion=dat.exp2$time.completion, group.number=dat.exp2$group.number))
 levene = levene.test(dat.exp2.levene$time.completion, dat.exp2.levene$group.number)
 kruskal = kruskal.test(dat.exp2$time.completion~dat.exp2$group.number)
-grp1 = dat.exp2[dat.exp2$group.number == 1,]$time.completion
-grp2 = dat.exp2[dat.exp2$group.number == 2,]$time.completion
-grp = cbind(grp1, grp2)
-friedman = friedman.test(grp)
 out.anova = kruskal2tex(kruskal)
 out.file.anova = gsub(".csv", "-anova.tex", out.file.boxplot)
 write(
