33e42f6fed6cf5fb53077bfca3a34519757590c8
Lapin <juste.lapin@gmail.com>
2011-07-05 10:21:47 +0200

    Add speed graphic for experimentation 2
    Add a graphic about mean speed of monome and binome for each try on the second experimentation.

---
+++ files/exp2-speed-try-group.r
@@ -0,0 +1,58 @@
+#!/usr/bin/env Rscript
+setwd("./files/")
+source("Rinit")
+out.file.boxplot = get.outname(commandArgs())
+
+dat.speed.monome = dat.exp2[dat.exp2$group.number == 1, ]
+bp.monome = boxplot(dat.speed.monome$mean.speed~dat.speed.monome$group.try, plot=FALSE)
+colnames(bp.monome$stats) = c("1","2","3")
+write.table(bp.monome$stats,
+			file = out.file.boxplot,
+			quote = FALSE,
+			sep = ",",
+			eol = "\n",
+			dec = ".",
+			row.names = FALSE,
+			col.names = TRUE)
+dat.speed.binome = dat.exp2[dat.exp2$group.number == 2, ]
+bp.binome = boxplot(dat.speed.binome$mean.speed~dat.speed.binome$group.try, plot=FALSE)
+write.table(bp.binome$stats,
+			file = out.file.boxplot,
+			quote = FALSE,
+			sep = ",",
+			eol = "\n",
+			dec = ".",
+			row.names = FALSE,
+			col.names = FALSE,
+			append = TRUE)
+
+dat.speed.try1 = dat.exp2[dat.exp2$group.try == 1, ]
+shapiro = shapiro.test(dat.speed.try1$mean.speed)
+levene = levene.test(dat.speed.try1$mean.speed, dat.speed.try1$group.number)
+kruskal = kruskal.test(dat.speed.try1$mean.speed~dat.speed.try1$group.number)
+out.anova = kruskal2tex(kruskal)
+out.file.anova = gsub(".csv", "-anova-try1.tex", out.file.boxplot)
+write(
+	  out.anova,
+	  file = out.file.anova
+	  )
+dat.speed.try2 = dat.exp2[dat.exp2$group.try == 2, ]
+shapiro = shapiro.test(dat.speed.try2$mean.speed)
+levene = levene.test(dat.speed.try2$mean.speed, dat.speed.try2$group.number)
+kruskal = kruskal.test(dat.speed.try2$mean.speed~dat.speed.try2$group.number)
+out.anova = kruskal2tex(kruskal)
+out.file.anova = gsub(".csv", "-anova-try2.tex", out.file.boxplot)
+write(
+	  out.anova,
+	  file = out.file.anova
+	  )
+dat.speed.try3 = dat.exp2[dat.exp2$group.try == 3, ]
+shapiro = shapiro.test(dat.speed.try3$mean.speed)
+levene = levene.test(dat.speed.try3$mean.speed, dat.speed.try3$group.number)
+kruskal = kruskal.test(dat.speed.try3$mean.speed~dat.speed.try3$group.number)
+out.anova = kruskal2tex(kruskal)
+out.file.anova = gsub(".csv", "-anova-try3.tex", out.file.boxplot)
+write(
+	  out.anova,
+	  file = out.file.anova
+	  )
