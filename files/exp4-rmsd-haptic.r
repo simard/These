ae56b5440261cd2833f4a1bbcc12410bec8c1813
Lapin <juste.lapin@gmail.com>
2011-10-20 17:51:07 +0200

    RMSD/haptic graphic experimentation 4
    Add a graphic about the minimum RMSD for groups depending on the presence of haptic on the fourth experimentation.

---
+++ files/exp4-rmsd-haptic.r
@@ -0,0 +1,32 @@
+#!/usr/bin/env Rscript
+setwd("./files/")
+source("Rinit")
+out.file.boxplot = get.outname(commandArgs())
+
+dat = aggregate(
+				dat.exp4,
+				by = list(
+						  id = dat.exp4$id,
+						  molecule = dat.exp4$molecule,
+						  haptic = dat.exp4$haptic
+						  ),
+				smart.mean
+				)
+bp = boxplot(dat$min.rmsd ~ dat$haptic, plot=FALSE)
+colnames(bp$stats) = c("Sans","Avec")
+write.table(bp$stats,
+			file = out.file.boxplot,
+			quote = FALSE,
+			sep = ",",
+			eol = "\n",
+			dec = ".",
+			row.names = FALSE,
+			col.names = TRUE)
+
+wilcox = wilcox.test(dat$min.rmsd ~ dat$haptic)
+out.anova = wilcox2tex(wilcox)
+out.file.anova = gsub(".csv", "-anova.tex", out.file.boxplot)
+write(
+	  out.anova,
+	  file = out.file.anova
+	  )
