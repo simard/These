346d9439ebfd921049e6f2aecbcff301aee172fb
Lapin <juste.lapin@gmail.com>
2011-10-20 18:51:01 +0200

    RMSD-time/haptic/mol graphic experimentation 4
    Add a graphic about the time of acceptation for designation for operators depending on the presence of haptic for each molecule on the fourth experimentation.

---
+++ files/exp4-rmsd-time-haptic-ubiquitin.r
@@ -0,0 +1,33 @@
+#!/usr/bin/env Rscript
+setwd("./files/")
+source("Rinit")
+out.file.boxplot = get.outname(commandArgs())
+
+dat.agg = aggregate(
+					dat.exp4,
+					by = list(
+							  id = dat.exp4$id,
+							  molecule = dat.exp4$molecule,
+							  haptic = dat.exp4$haptic
+							  ),
+					smart.mean
+					)
+dat = dat.agg[dat.agg$molecule == "ubiquitin",]
+bp = boxplot(dat$min.rmsd.time ~ dat$haptic, plot=FALSE)
+colnames(bp$stats) = c("Sans","Avec")
+write.table(bp$stats,
+			file = out.file.boxplot,
+			quote = FALSE,
+			sep = ",",
+			eol = "\n",
+			dec = ".",
+			row.names = FALSE,
+			col.names = TRUE)
+
+wilcox = wilcox.test(dat$min.rmsd.time ~ dat$haptic)
+out.anova = wilcox2tex(wilcox)
+out.file.anova = gsub(".csv", "-anova.tex", out.file.boxplot)
+write(
+	  out.anova,
+	  file = out.file.anova
+	  )
