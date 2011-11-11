051476ba8e6cbe24d146f541646042542ff83185
Lapin <juste.lapin@gmail.com>
2011-11-11 17:05:53 +0100

    Annotation/haptic graphic experimentation 4
    Add a graphic about the time of oral communication of the subjects depending on the presence of haptic on the fourth experimentation.

---
+++ files/exp4-annotation-time-haptic.r
@@ -0,0 +1,27 @@
+#!/usr/bin/env Rscript
+setwd("./files/")
+source("Rinit")
+out.file.boxplot = get.outname(commandArgs())
+
+dat = dat.exp4
+bp = boxplot(dat$annotation.time ~ dat$haptic, plot=FALSE)
+colnames(bp$stats) = c("Sans","Avec")
+write.table(bp$stats,
+			file = out.file.boxplot,
+			quote = FALSE,
+			sep = ",",
+			eol = "\n",
+			dec = ".",
+			row.names = FALSE,
+			col.names = TRUE)
+
+a = dat[ dat$haptic == "True", ]$annotation.time
+b = dat[ dat$haptic == "False", ]$annotation.time
+wilcox = wilcox.test( dat$annotation.time ~ dat$haptic, paired = TRUE )
+wilcox = wilcox.test( a, b, paired = TRUE )
+out.anova = wilcox2tex(wilcox)
+out.file.anova = gsub(".csv", "-anova.tex", out.file.boxplot)
+write(
+	  out.anova,
+	  file = out.file.anova
+	  )
