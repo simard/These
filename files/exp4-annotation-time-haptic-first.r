deb2ead4c94f26b4f0d63ecf20200913b37bf433
Lapin <juste.lapin@gmail.com>
2011-11-12 23:28:50 +0100

    Review section 2 of results in experimentation 4
    The second section of results in the fourth experimentation has been reviewed.
    Moreover, discussion about the time of speaking has been added.

--- files/exp4-annotation-time-haptic-first.r
+++ files/exp4-annotation-time-haptic-first.r
@@ -4,7 +4,7 @@ source("Rinit")
 out.file.boxplot = get.outname(commandArgs())
 
 dat = dat.exp4
-bp = boxplot(dat$annotation.time ~ dat$haptic.last, plot=FALSE)
+bp = boxplot(dat$annotation.time ~ dat$haptic.first, plot=FALSE)
 colnames(bp$stats) = c("Sans/Avec","Avec/Sans")
 write.table(bp$stats,
 			file = out.file.boxplot,
@@ -15,9 +15,7 @@ write.table(bp$stats,
 			row.names = FALSE,
 			col.names = TRUE)
 
-a = dat[ dat$haptic.last == "True", ]$annotation.time
-b = dat[ dat$haptic.last == "False", ]$annotation.time
-wilcox = wilcox.test( dat$annotation.time ~ dat$haptic.last, na.action = "na.pass" )
+wilcox = wilcox.test( dat$annotation.time ~ dat$haptic.first, na.action = "na.pass" )
 out.anova = wilcox2tex(wilcox)
 out.file.anova = gsub(".csv", "-anova.tex", out.file.boxplot)
 write(
